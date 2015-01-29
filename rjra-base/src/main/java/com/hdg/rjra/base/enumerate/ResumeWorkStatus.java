f8a48afc1d50d3ac5ea08149e6f94bc172dbba91
rock.zhu <rock.zhu@ndpmedia.com>
2015-01-29 17:52:45 +0800

    修改枚举信息

--- rjra-base/src/main/java/com/hdg/rjra/base/enumerate/ResumeWorkStatus.java
+++ rjra-base/src/main/java/com/hdg/rjra/base/enumerate/ResumeWorkStatus.java
@@ -1,14 +1,15 @@
 package com.hdg.rjra.base.enumerate;
 
 /**
- * Created by Rock on 2015/1/8 0008.
+ * Created by Rock on 2015/1/29 0029.
  */
-public enum UserWorkStatus {
+public enum ResumeWorkStatus {
     Idle(0),
-    Working(1);
+    Working(1),
+    WorkingWantChange(2);
 
     private int code;
-    private UserWorkStatus(int code) {
+    private ResumeWorkStatus(int code) {
         this.code = code;
     }
 
