2ec0c76e970c6dc235258a95da67d0c180d5eac6
rock.zhu <rock.zhu@5igdd.com>
2015-03-21 09:39:14 +0800

    0.09 邀请save接口调整

--- rjra-rdb/rjra-rdb-executer/src/main/java/com/hdg/rjra/rdb/executer/userinviteuser/SaveUserInviteUser.java
+++ rjra-rdb/rjra-rdb-executer/src/main/java/com/hdg/rjra/rdb/executer/userinviteuser/SaveUserInviteUser.java
@@ -14,7 +14,9 @@ import java.util.Date;
  */
 public class SaveUserInviteUser extends AbstractExecuter {
 
-    private String sql = "insert into user_invite_user(user_id,invite_user_id,invite_time,user_invite_user_status) values (?,?,?,?)";
+    private String sql = "insert into user_invite_user(user_id,invite_user_id,invite_time," +
+            "user_invite_user_status,interview_time,interview_address,interview_mobile,interview_desc," +
+            "apply_id,interviewer) values (?,?,?,?,?,?,?,?,?)";
 
     @Override
     public Object execute(Object[] params) {
@@ -26,6 +28,12 @@ public class SaveUserInviteUser extends AbstractExecuter {
                 ps.setObject(2, userInviteUser.getInviteUserId());
                 ps.setObject(3, new Date());
                 ps.setObject(4, UserInviteUserStatus.Active.getCode());
+                ps.setObject(5, userInviteUser.getInterviewTime());
+                ps.setObject(6, userInviteUser.getInterviewAddress());
+                ps.setObject(7, userInviteUser.getInterviewMobile());
+                ps.setObject(8, userInviteUser.getInterviewDesc());
+                ps.setObject(9, userInviteUser.getApplyId());
+                ps.setObject(10, userInviteUser.getInterviewer());
             }
         };
         return saveResultId(sql, pst);
--- rjra-server/rjra-server-web/src/main/java/com/hdg/rjra/server/web/controller/param/userbehavior/UserInviteUserParam.java
+++ rjra-server/rjra-server-web/src/main/java/com/hdg/rjra/server/web/controller/param/userbehavior/UserInviteUserParam.java
@@ -11,6 +11,13 @@ public class UserInviteUserParam extends BaseParam implements Serializable {
     private Long inviteId;
     private Long userId;
     private Long inviteUserId;
+    private String interviewTime;
+    private String interviewAddress;
+    private String interviewer;
+    private String interviewMobile;
+    private String interviewDesc;
+    private Long applyId;
+    private Integer userInviteUserStatus;
 
     public Long getInviteId() {
         return inviteId;
@@ -34,5 +41,61 @@ public class UserInviteUserParam extends BaseParam implements Serializable {
 
     public void setInviteUserId(Long inviteUserId) {
         this.inviteUserId = inviteUserId;
+    }
+
+    public String getInterviewTime() {
+        return interviewTime;
+    }
+
+    public void setInterviewTime(String interviewTime) {
+        this.interviewTime = interviewTime;
+    }
+
+    public String getInterviewAddress() {
+        return interviewAddress;
+    }
+
+    public void setInterviewAddress(String interviewAddress) {
+        this.interviewAddress = interviewAddress;
+    }
+
+    public String getInterviewer() {
+        return interviewer;
+    }
+
+    public void setInterviewer(String interviewer) {
+        this.interviewer = interviewer;
+    }
+
+    public String getInterviewMobile() {
+        return interviewMobile;
+    }
+
+    public void setInterviewMobile(String interviewMobile) {
+        this.interviewMobile = interviewMobile;
+    }
+
+    public String getInterviewDesc() {
+        return interviewDesc;
+    }
+
+    public void setInterviewDesc(String interviewDesc) {
+        this.interviewDesc = interviewDesc;
+    }
+
+    public Long getApplyId() {
+        return applyId;
+    }
+
+    public void setApplyId(Long applyId) {
+        this.applyId = applyId;
+    }
+
+    public Integer getUserInviteUserStatus() {
+        return userInviteUserStatus;
+    }
+
+    public void setUserInviteUserStatus(Integer userInviteUserStatus) {
+        this.userInviteUserStatus = userInviteUserStatus;
     }
 }
