c0c3f68c89aa880ef0f5607453235451edbc2dc3
rock.zhu <rock.zhu@5igdd.com>
2015-03-24 16:09:28 +0800

    0.09 通用update方法

--- rjra-rdb/rjra-rdb-executer/src/main/java/com/hdg/rjra/rdb/executer/examinelog/SaveExamineLog.java
+++ rjra-rdb/rjra-rdb-executer/src/main/java/com/hdg/rjra/rdb/executer/examinelog/SaveExamineLog.java
@@ -1,7 +1,16 @@
 package com.hdg.rjra.rdb.executer.examinelog;
 
+import com.hdg.rjra.rdb.executer.AbstractExecuter;
+
 /**
  * Created by Administrator on 2015/3/24.
  */
-public class SaveExamineLog {
+public class SaveExamineLog extends AbstractExecuter {
+
+    static String sql = "select * from base_company_type";
+
+    @Override
+    public Object execute(Object[] params) {
+        return getJdbcTemplate().query(sql, new Object[]{}, rowMapper);
+    }
 }
