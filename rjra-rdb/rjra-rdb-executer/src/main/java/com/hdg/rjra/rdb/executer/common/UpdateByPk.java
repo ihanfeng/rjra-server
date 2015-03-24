7fc34c09f393ca5c45848b5f0f87a087078b92ba
rock.zhu <rock.zhu@5igdd.com>
2015-03-24 18:33:00 +0800

    内部0.02审核功能开发

--- rjra-rdb/rjra-rdb-executer/src/main/java/com/hdg/rjra/rdb/executer/common/UpdateByPk.java
+++ rjra-rdb/rjra-rdb-executer/src/main/java/com/hdg/rjra/rdb/executer/common/UpdateByPk.java
@@ -1,12 +1,18 @@
 package com.hdg.rjra.rdb.executer.common;
 
 import com.hdg.rjra.rdb.executer.AbstractExecuter;
+import com.hdg.rjra.rdb.model.thrift.ResultType;
 import com.hdg.rjra.rdb.proxy.domain.BaseDomain;
 import com.hdg.rjra.rdb.proxy.utils.SqlParam;
 import com.hdg.rjra.rdb.proxy.utils.SqlUtils;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by Administrator on 2015/3/23.
@@ -15,8 +21,24 @@ public class UpdateByPk extends AbstractExecuter {
 
     @Override
     public Object execute(Object[] params) {
-        BaseDomain baseDomain = (BaseDomain) params[0];
-        SqlParam sqlParam = SqlUtils.buildUpdateSqlByDomain(baseDomain);
-        return getJdbcTemplate().update(sqlParam.getSql(), sqlParam.getObjects());
+        if (params != null && params.length > 0) {
+            BaseDomain baseDomain = (BaseDomain) params[0];
+            SqlParam sqlParam = SqlUtils.buildUpdateSqlByDomain(baseDomain);
+            final String sql = sqlParam.getSql();
+            final List<Object> objects = sqlParam.getObjects();
+            getJdbcTemplate().update(new PreparedStatementCreator() {
+                public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                    PreparedStatement ps = con.prepareStatement(sql);
+                    for (int i = 0; i < objects.size(); i++) {
+                        ps.setObject(i + 1, objects.get(i));
+                    }
+                    return ps;
+                }
+            });
+            return Integer.valueOf(ResultType.SUCCESS.getCode());
+        }else{
+            return Integer.valueOf(ResultType.PARAM_ERROR.getCode());
+        }
+
     }
 }
