b802c0b14f61a964292bf4a3be56a64835b28b7b
rock.zhu <rock.zhu@5igdd.com>
2015-03-28 12:08:53 +0800

    0.0.3 web

--- rjra-server/rjra-server-serviceimpl/src/main/java/com/hdg/rjra/server/serviceimpl/importlog/ImportLogServiceImpl.java
+++ rjra-server/rjra-server-serviceimpl/src/main/java/com/hdg/rjra/server/serviceimpl/importlog/ImportLogServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hdg.rjra.server.serviceimpl.importlog;
 
+import com.hdg.rjra.server.model.bo.importlog.ImportData;
 import com.hdg.rjra.server.serviceimpl.importlog.BaseImportLogServiceImpl;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +12,58 @@ import org.springframework.stereotype.Service;
 public class ImportLogServiceImpl extends BaseImportLogServiceImpl {
 
 
+    @Override
+    protected Long parseCompanyProvinceId(ImportData importData) {
+        return null;
+    }
+
+    @Override
+    protected Long parseCompanyCityId(ImportData importData) {
+        return null;
+    }
+
+    @Override
+    protected Long parseCompanyAreaId(ImportData importData) {
+        return null;
+    }
+
+    @Override
+    protected Long parseCompanyScale(ImportData importData) {
+        return null;
+    }
+
+    @Override
+    protected Long parseCompanyType(ImportData importData) {
+        return null;
+    }
+
+    @Override
+    protected String parseCompanyDesc(ImportData importData) {
+        return null;
+    }
+
+    @Override
+    protected String parseCompanyAddress(ImportData importData) {
+        return null;
+    }
+
+    @Override
+    protected String parseCompanyContactEmail(ImportData importData) {
+        return null;
+    }
+
+    @Override
+    protected String parseCompanyContactMobile(ImportData importData) {
+        return null;
+    }
+
+    @Override
+    protected String parseCompanyContact(ImportData importData) {
+        return null;
+    }
+
+    @Override
+    protected String parseCompanyName(ImportData importData) {
+        return null;
+    }
 }
